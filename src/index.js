45a7ccb3d3bf1906b4b7b953e71c8dd5f3cda7d6
Margarita <>
2019-03-23 23:32:53 +0300

    first solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function warmup(temperature) {
-  // your implementation here
-};
+function warmup(temp) {
+    return (temp * ( 9/5 * 1000 ) / 1000 ) + 32
+}
+
+var a = warmup(5);
+console.log(' 5C in Farenheit is ' + a + 'F');
